0efcaf5fac10055fc1c5e583ffe0b444a5636544
LaPortaM43 <136745957+LaPortaM43@users.noreply.github.com>
2023-09-13 21:32:07 -0400

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,16 @@
+package Monmouth;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+			
+		System.out.println("\\(>_<) \n  ))z"); 
+		
+		System.out.println("  / \\");
+			
+			
+			
+		}
+	}
+
+
